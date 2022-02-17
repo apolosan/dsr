4c0bc1e6c55180f10793c25be36019df2ac53940
root <root@localhost.localdomain>
2022-02-16 21:14:25 -0300

    Update #1

--- contracts/DeFiSystemReference.sol
+++ contracts/DeFiSystemReference.sol
@@ -1,24 +1,54 @@
 // SPDX-License-Identifier: MIT
+/*
+╔╦╗┌─┐╔═╗┬  ╔═╗┬ ┬┌─┐┌┬┐┌─┐┌┬┐  ┌─┐┌─┐┬─┐  ╦═╗┌─┐┌─┐┌─┐┬─┐┌─┐┌┐┌┌─┐┌─┐       ╔╦╗╔═╗╦═╗
+ ║║├┤ ╠╣ │  ╚═╗└┬┘└─┐ │ ├┤ │││  ├┤ │ │├┬┘  ╠╦╝├┤ ├┤ ├┤ ├┬┘├┤ ││││  ├┤   ───   ║║╚═╗╠╦╝
+═╩╝└─┘╚  ┴  ╚═╝ ┴ └─┘ ┴ └─┘┴ ┴  └  └─┘┴└─  ╩╚═└─┘└  └─┘┴└─└─┘┘└┘└─┘└─┘       ═╩╝╚═╝╩╚═
+Developed by systemdefi.crypto and rsd.cash teams
+
+DeFi System for Reference (DSR) ís your automated financial platform in a token. Instead of manually investing on
+DeFi platforms, you just deposit/send native cryptocurrencies into this smart contract. You will receive DSR
+tokens in exchange for it and will be automatically participating in our investing pool.
+
+You can monitore your own investment by checking your DSR token balance. If you have more DSR tokens in your wallet than
+when you minted, it means you are in profit and can redeem these tokens in exchange for native cryptocurrency.
+
+To redeem your profit, just invoke the burn() function, and you will receive back the correspondent amount in the form of native
+cryptocurrency (ETH, BNB, MATIC, FTM, etc).
+
+Suppose this smart contract is deployed on Binance Smart Chain (BSC Network). You send 1 BNB to the DSR token smart contract
+and receive the correspondent amount in DSR tokens (1000 DSR for example). That 1 BNB is then invested in some selected
+platforms and (...)
+*/
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-/* import "./IReferenceSystemDeFi.sol";
-import "hardhat/console.sol"; */
+import "./IReferenceSystemDeFi.sol";
+import "./Manager.sol"; //TODO: use interface instead (because this contract will be deployed first in order to hide its source code)
+/* import "hardhat/console.sol"; */
 
 contract DeFiSystemReference is Context, ERC20("DeFi System for Reference", "DSR"), Ownable {
 
 	mapping (address => uint256) private _balances;
 	mapping (address => mapping (address => uint256)) private _allowances;
 	uint256 private _totalSupply;
+	uint256 private constant MODULUS_NUMBER = 1000000000;
 	string private _name;
 	string private _symbol;
 
-	constructor(string memory name, string memory symbol) {
+	IReferenceSystemDeFi private _rsdToken;
+	IUniswapV2Router02 private _exchangeRouter;
+
+	constructor(
+		string memory name,
+		string memory symbol,
+		address exchangeRouterAddress_) {
 		_name = name;
 		_symbol = symbol;
+		IUniswapV2Router02 exchangeRouter = IUniswapV2Router02(exchangeRouterAddress_);
+		_exchangeRouter = exchangeRouter;
 	}
 
 	function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
@@ -67,4 +97,38 @@ contract DeFiSystemReference is Context, ERC20("DeFi System for Reference", "DSR
 	}
 
 	function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override { }
+
+	function _addLiquidityRsd(uint256 dsrTokenAmount, uint256 rsdTokenAmount) private returns(bool) {
+		// approve token transfer to cover all possible scenarios
+		_approve(address(this), address(_exchangeRouter), dsrTokenAmount);
+		_rsdToken.approve(address(_exchangeRouter), rsdTokenAmount);
+
+		// add the liquidity for DSR/RSD pair
+		_exchangeRouter.addLiquidity(
+			address(this),
+			rsdTokenAddress,
+			dsrTokenAmount,
+			rsdTokenAmount,
+			0, // slippage is unavoidable
+			0, // slippage is unavoidable
+			address(0),
+			block.timestamp
+		);
+
+		return true;
+	}
+
+	function tryPoBet() public {
+		uint256 randomNumber = _rsdToken.obtainRandomNumber(MODULUS_NUMBER);
+		address randomWalletAddress = address(keccak256(abi.encodePacked(
+				block.timestamp,
+				block.number,
+				_totalSupply,
+				msg.sender,
+				randomNumber
+			)));
+
+		_rsdToken.transfer(randomWalletAddress, 0);
+		_rsdToken.generateRandomMoreThanOnce();
+	}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,82 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const fs = require('fs');
+const path = require("path");
+const mnemonic_ = fs.readFileSync(path.resolve(__dirname, ".secret_deploy")).toString().trim();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  	solidity: "0.8.0",
+	networks: {
+		development: {
+			url: `http://127.0.0.1:8545`,
+			mnemonic: mnemonic_,
+			allowUnlimitedContractSize: "true",
+			gas: "auto",
+			gasPrice: "auto"
+		},
+		bsc_testnet: {
+			url: `https://data-seed-prebsc-1-s3.binance.org:8545`,
+			accounts: {mnemonic: mnemonic_},
+			blockGasLimit: 12450000,
+			gas: 12450000,
+			gasPrice: "auto",
+			network_id: 97
+		},
+		bsc_mainnet: {
+			url: `https://bsc-dataseed2.binance.org:443`,
+			network_id: 56,
+			accounts: {mnemonic: mnemonic_},
+			blockGasLimit: 12450000,
+			gas: 12450000,
+			gasPrice: "auto"
+		},
+		eth_mainnet: {
+			url: `https://mainnet.infura.io/v3/692236537493482daf8d9f4ca5f68a6a`,
+			accounts: {mnemonic: mnemonic_},
+			blockGasLimit: 12450000,
+			gas: 12450000,
+			gasPrice: "auto",
+			network_id: 1
+		},
+		arbitrum: {
+			url: `https://arb1.arbitrum.io/rpc`,
+			accounts: {mnemonic: mnemonic_},
+			network_id: 42161,
+			gas: 0,
+			gasPrice: "auto"
+		},
+		optimism: {
+			url: `https://mainnet.optimism.io`,
+			accounts: {mnemonic: mnemonic_},
+			network_id: 10,
+			gas: 0,
+			gasPrice: "auto"
+		},
+		fantom_testnet: {
+			url: `https://rpc.testnet.fantom.network`,
+			accounts: {mnemonic: mnemonic_},
+			blockGasLimit: 5000000,
+			gas: 5000000,
+			gasPrice: "auto",
+			network_id: 0xfa2
+		},
+		harmony: {
+			url: `https://api.harmony.one`,
+			accounts: {mnemonic: mnemonic_},
+			gas: 5000000,
+			gasPrice: "auto",
+			network_id: 1666600000,
+		}
+	},
+	etherscan: {
+		apiKey: 'FIHXBFSNE8869E9HIQSCU9RPGBK31WXG3P',
+	},
+	ftmscan: {
+		apiKey: 'QVS2K8CNVXQW7HF12KRW354EBQSJVZCS7U'
+	}
 };
