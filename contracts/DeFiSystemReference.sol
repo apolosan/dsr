c2a402b7aca0ecb04c39c2015fb61ac9353959b2
root <root@localhost.localdomain>
2022-02-17 19:02:26 -0300

    Update #2

--- contracts/DeFiSystemReference.sol
+++ contracts/DeFiSystemReference.sol
@@ -4,7 +4,7 @@
  ║║├┤ ╠╣ │  ╚═╗└┬┘└─┐ │ ├┤ │││  ├┤ │ │├┬┘  ╠╦╝├┤ ├┤ ├┤ ├┬┘├┤ ││││  ├┤   ───   ║║╚═╗╠╦╝
 ═╩╝└─┘╚  ┴  ╚═╝ ┴ └─┘ ┴ └─┘┴ ┴  └  └─┘┴└─  ╩╚═└─┘└  └─┘┴└─└─┘┘└┘└─┘└─┘       ═╩╝╚═╝╩╚═
 Developed by systemdefi.crypto and rsd.cash teams
-
+---------------------------------------------------------------------------------------
 DeFi System for Reference (DSR) ís your automated financial platform in a token. Instead of manually investing on
 DeFi platforms, you just deposit/send native cryptocurrencies into this smart contract. You will receive DSR
 tokens in exchange for it and will be automatically participating in our investing pool.
@@ -18,6 +18,10 @@ cryptocurrency (ETH, BNB, MATIC, FTM, etc).
 Suppose this smart contract is deployed on Binance Smart Chain (BSC Network). You send 1 BNB to the DSR token smart contract
 and receive the correspondent amount in DSR tokens (1000 DSR for example). That 1 BNB is then invested in some selected
 platforms and (...)
+
+DSR is the third token of the triad system: RSD | SDR | DSR. It also reserver some profit amount to buy RSD and SDR tokens
+while locks liquidity for correpondent pairs in the main exchanges.
+---------------------------------------------------------------------------------------
 */
 pragma solidity >=0.8.0;
 
@@ -26,29 +30,50 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IReferenceSystemDeFi.sol";
+import "./IUniswapV2Router02.sol";
+import "./IUniswapV2Factory.sol";
+import "./IWETH.sol";
 import "./Manager.sol"; //TODO: use interface instead (because this contract will be deployed first in order to hide its source code)
 /* import "hardhat/console.sol"; */
 
 contract DeFiSystemReference is Context, ERC20("DeFi System for Reference", "DSR"), Ownable {
 
+	using SafeMath for uint256;
+
+	address public rsdTokenAddress;
+	address public sdrTokenAddress;
+
+	address public dsrEthPair;
+	address public dsrRsdPair;
+	address public dsrSdrPair;
+	address public rsdEthPair;
+	address public sdrRsdPair;
+
+	uint256 private _countTryPoBet;
+	uint256 private _totalSupply;
+
 	mapping (address => uint256) private _balances;
 	mapping (address => mapping (address => uint256)) private _allowances;
-	uint256 private _totalSupply;
-	uint256 private constant MODULUS_NUMBER = 1000000000;
+
 	string private _name;
 	string private _symbol;
 
+	IERC20 private _sdrToken;
+	IWETH private _wEth;
 	IReferenceSystemDeFi private _rsdToken;
 	IUniswapV2Router02 private _exchangeRouter;
 
+	modifier lockTryPoBet {
+		_countTryPoBet++;
+		_;
+		_countTryPoBet--;
+	}
+
 	constructor(
 		string memory name,
-		string memory symbol,
-		address exchangeRouterAddress_) {
+		string memory symbol) {
 		_name = name;
 		_symbol = symbol;
-		IUniswapV2Router02 exchangeRouter = IUniswapV2Router02(exchangeRouterAddress_);
-		_exchangeRouter = exchangeRouter;
 	}
 
 	function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
@@ -98,7 +123,26 @@ contract DeFiSystemReference is Context, ERC20("DeFi System for Reference", "DSR
 
 	function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override { }
 
-	function _addLiquidityRsd(uint256 dsrTokenAmount, uint256 rsdTokenAmount) private returns(bool) {
+	function balanceOf(address account) public view virtual override returns (uint256) {
+			return _balances[account];
+	}
+
+	function _addLiquidityDsrEth(uint256 dsrTokenAmount, uint256 ethAmount) private returns(bool) {
+			_approve(address(this), address(_exchangeRouter), dsrTokenAmount);
+			// add the liquidity for DSR/ETH pair
+			_exchangeRouter.addLiquidityETH{value: ethAmount}(
+					address(this),
+					dsrTokenAmount,
+					0, // slippage is unavoidable
+					0, // slippage is unavoidable
+					address(0),
+					block.timestamp
+			);
+
+			return true;
+	}
+
+	function _addLiquidityDsrRsd(uint256 dsrTokenAmount, uint256 rsdTokenAmount) private returns(bool) {
 		// approve token transfer to cover all possible scenarios
 		_approve(address(this), address(_exchangeRouter), dsrTokenAmount);
 		_rsdToken.approve(address(_exchangeRouter), rsdTokenAmount);
@@ -118,17 +162,123 @@ contract DeFiSystemReference is Context, ERC20("DeFi System for Reference", "DSR
 		return true;
 	}
 
-	function tryPoBet() public {
-		uint256 randomNumber = _rsdToken.obtainRandomNumber(MODULUS_NUMBER);
-		address randomWalletAddress = address(keccak256(abi.encodePacked(
+	function _addLiquidityDsrSdr(uint256 dsrTokenAmount, uint256 sdrTokenAmount) private returns(bool) {
+		// approve token transfer to cover all possible scenarios
+		_approve(address(this), address(_exchangeRouter), dsrTokenAmount);
+		_sdrToken.approve(address(_exchangeRouter), sdrTokenAmount);
+
+		// add the liquidity for DSR/SDR pair
+		_exchangeRouter.addLiquidity(
+			address(this),
+			sdrTokenAddress,
+			dsrTokenAmount,
+			sdrTokenAmount,
+			0, // slippage is unavoidable
+			0, // slippage is unavoidable
+			address(0),
+			block.timestamp
+		);
+
+		return true;
+	}
+
+	function _getDsrRsdPoolRate() private view returns(uint256) {
+		if (address(dsrRsdPair) == address(0)) {
+			return 1;
+		} else {
+			uint256 rsdBalance = _rsdToken.balanceOf(dsrRsdPair);
+			uint256 dsrBalance = balanceOf(dsrRsdPair);
+			dsrBalance = dsrBalance == 0 ? 1 : dsrBalance;
+			return (rsdBalance.div(dsrBalance));
+		}
+	}
+
+	function _getDsrSdrPoolRate() private view returns(uint256) {
+		if (address(dsrSdrPair) == address(0)) {
+			return 1;
+		} else {
+			uint256 sdrBalance = _sdrToken.balanceOf(dsrSdrPair);
+			uint256 dsrBalance = balanceOf(dsrSdrPair);
+			dsrBalance = dsrBalance == 0 ? 1 : dsrBalance;
+			return (sdrBalance.div(dsrBalance));
+		}
+	}
+
+	function initializeTokenContract(
+			address exchangeRouterAddress_,
+			address rsdTokenAddress_,
+			address sdrTokenAddress_
+		) public onlyOwner {
+			IUniswapV2Router02 exchangeRouter = IUniswapV2Router02(exchangeRouterAddress_);
+			_exchangeRouter = exchangeRouter;
+			rsdTokenAddress = rsdTokenAddress_; // 0x61ed1c66239d29cc93c8597c6167159e8f69a823
+			sdrTokenAddress = sdrTokenAddress_;
+			_rsdToken = IReferenceSystemDeFi(rsdTokenAddress);
+
+			_wEth = IWETH(exchangeRouter.WETH());
+
+			address _dsrEthPair = IUniswapV2Factory(exchangeRouter.factory()).getPair(address(this), address(_wEth));
+			if (_dsrEthPair == address(0))
+				_dsrEthPair = IUniswapV2Factory(exchangeRouter.factory()).createPair(address(this), address(_wEth));
+			dsrEthPair = _dsrEthPair;
+
+			address _dsrRsdPair = IUniswapV2Factory(exchangeRouter.factory()).getPair(address(this), rsdTokenAddress);
+			if (_dsrRsdPair == address(0))
+				_dsrRsdPair = IUniswapV2Factory(exchangeRouter.factory()).createPair(address(this), rsdTokenAddress);
+			dsrRsdPair = _dsrRsdPair;
+
+			address _dsrSdrPair = IUniswapV2Factory(exchangeRouter.factory()).getPair(address(this), sdrTokenAddress);
+			if (_dsrSdrPair == address(0))
+				_dsrSdrPair = IUniswapV2Factory(exchangeRouter.factory()).createPair(address(this), sdrTokenAddress);
+			dsrSdrPair = _dsrSdrPair;
+
+			address _rsdEthPair = IUniswapV2Factory(exchangeRouter.factory()).getPair(rsdTokenAddress, address(_wEth));
+			if (_rsdEthPair == address(0))
+				_rsdEthPair = IUniswapV2Factory(exchangeRouter.factory()).createPair(rsdTokenAddress, address(_wEth));
+			rsdEthPair = _rsdEthPair;
+
+			address _sdrRsdPair = IUniswapV2Factory(exchangeRouter.factory()).getPair(sdrTokenAddress, rsdTokenAddress);
+			if (_sdrRsdPair == address(0))
+				_sdrRsdPair = IUniswapV2Factory(exchangeRouter.factory()).createPair(sdrTokenAddress, rsdTokenAddress);
+			sdrRsdPair = _sdrRsdPair;
+	}
+
+	function obtainRandomWalletAddress() public view returns(address) {
+		uint256 someValue = _rsdToken.totalSupply();
+		address randomWalletAddress = address(bytes20(sha256(abi.encodePacked(
 				block.timestamp,
 				block.number,
 				_totalSupply,
 				msg.sender,
-				randomNumber
-			)));
+				someValue
+			))));
+		return randomWalletAddress;
+	}
 
-		_rsdToken.transfer(randomWalletAddress, 0);
+	function tryPoBet() public lockTryPoBet {
+		require(_countTryPoBet < 2, "DSR: it is not allowed to call this function more than once");
+		uint256 rsdBalance = _rsdToken.balanceOf(address(this));
+
+		if (rsdBalance > 0)
+			_rsdToken.transfer(obtainRandomWalletAddress(), 1);
+		else
+			_rsdToken.transfer(obtainRandomWalletAddress(), 0);
+
+		if (rsdBalance < _rsdToken.balanceOf(address(this))) {
+			uint256 earnedRsd = _rsdToken.balanceOf(address(this)).sub(rsdBalance);
+			_mint(address(this), _getDsrRsdPoolRate().mul(earnedRsd));
+			_addLiquidityDsrRsd(balanceOf(address(this)), earnedRsd);
+		}
+
 		_rsdToken.generateRandomMoreThanOnce();
+		delete rsdBalance;
+	}
+
+	function setSdrTokenAddress(address sdrTokenAddress_) public onlyOwner {
+		sdrTokenAddress = sdrTokenAddress_;
+	}
+
+	function setRsdTokenAddress(address rsdTokenAddress_) public onlyOwner {
+		rsdTokenAddress = rsdTokenAddress_;
 	}
 }
